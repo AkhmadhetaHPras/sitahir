ab3121935a050c59f02faa5a29fa9588d2d711aa
AkhmadhetaHPras <akhmadhetahafid@gmail.com>
2022-05-28 18:05:13 +0700

    make controller and fetching dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,31 +2,77 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\AnggaranListrik;
 use App\Models\Anggota;
+use App\Models\BukuAir;
 use App\Models\Instalasi;
+use App\Models\Pengumuman;
 use App\Models\Pengurus;
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\View;
-use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        // what data
+        $month = Carbon::parse(now())->month;
+        $year = Carbon::parse(now())->year;
+
+        $kubikprev = BukuAir::select("bulan", "tahun", DB::raw("sum(kubik) as kubik"))
+            ->groupBy(DB::raw("bulan"), DB::raw('tahun'))
+            ->where('bulan', $month - 1)
+            ->where('tahun', $year)
+            ->get();
+
+        $kubiknow = BukuAir::select("bulan", "tahun", DB::raw("sum(kubik) as kubik"))
+            ->groupBy(DB::raw("bulan"), DB::raw('tahun'))
+            ->where('bulan', $month)
+            ->where('tahun', $year)
+            ->get();
+
+
+        $cartdump = BukuAir::select("bulan", "tahun", DB::raw("sum(kubik) as kubik"))
+            ->orderBy('tahun', 'desc')
+            ->orderBy('bulan', 'desc')
+            ->groupBy(DB::raw("bulan"), DB::raw('tahun'))
+            ->take(6)
+            ->get();
+
+        $cartmonth = array();
+        foreach ($cartdump as $c) {
+            array_push($cartmonth, substr(date('F', mktime(0, 0, 0, $c->bulan, 10)), 0, 3));
+        }
+
+        $cartkubik = array();
+        foreach ($cartdump as $c) {
+            array_push($cartkubik, $c->kubik);
+        }
+
+        $cartmonth = array_reverse($cartmonth);
+        $cartkubik = array_reverse($cartkubik);
+
+        $jumlah_anggota = Anggota::select(DB::raw("count(id) as jumlah"))->get();
+
+        $anggaran_listrik = AnggaranListrik::all()->last();
+
+        $pengumuman = Pengumuman::all();
+
+        $data = [$kubiknow, $kubikprev, $cartmonth, $cartkubik, $jumlah_anggota, $anggaran_listrik, $pengumuman];
+
         if (Auth::user()->hasRole('pengurus')) {
-            return view('dashboard');
+            return view('dashboard', compact('data'));
         } elseif (Auth::user()->hasRole('admin')) {
-            return view('dashboard');
+            return view('dashboard', compact('data'));
         } elseif (Auth::user()->hasRole('anggota')) {
             $userprofile = Anggota::where('id_users', Auth::user()->id)->with('user')->first();
             $instalasi = Instalasi::where('id_anggota', $userprofile->id)->first();
-            if ($instalasi->status != 'selesesai') {
+            if ($instalasi->status != 'selesai') {
                 return redirect('instalasi');
             }
-            return view('dashboard');
+            return view('dashboard', compact('data'));
         }
     }
 
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -14,21 +14,21 @@
                             <div class="card-body p-2">
                                 <div class="row">
                                     <h2 class="col-8 d-flex align-items-center mb-0 justify-content-center">
-                                        <b>1308 M3</b>
+                                        <b>{{ $data[0]->get(0)->kubik }} M3</b>
                                     </h2>
                                     <div class="col-4 align-middle">
-                                        <div class="month">Juni</div>
-                                        <div class="year">2021</div>
+                                        <div class="month">{{date('F', mktime(0, 0, 0, $data[0]->get(0)->bulan, 10))}}</div>
+                                        <div class="year">{{ $data[0]->get(0)->tahun }}</div>
                                     </div>
                                 </div>
                                 <div class="divider border-3 border-bottom border-warning my-2"></div>
                                 <div class="row">
                                     <h2 class="col-8 d-flex align-items-center mb-0 justify-content-center">
-                                        <b>1308 M3</b>
+                                        <b>{{ $data[1]->get(0)->kubik }} M3</b>
                                     </h2>
                                     <div class="col-4 align-middle">
-                                        <div class="month">Juni</div>
-                                        <div class="year">2021</div>
+                                        <div class="month">{{date('F', mktime(0, 0, 0, $data[1]->get(0)->bulan, 10))}}</div>
+                                        <div class="year">{{ $data[1]->get(0)->tahun }}</div>
                                     </div>
                                 </div>
                             </div>
@@ -43,7 +43,7 @@
                                 </div>
 
                                 <script>
-                                    const labels = ["Jan", "Feb", "Mar", "Apr", "Mei", "Jun"];
+                                    const labels = @json($data[2]);
 
                                     const data = {
                                         labels: labels,
@@ -51,7 +51,7 @@
                                             label: "Kubik Air",
                                             backgroundColor: "rgb(33, 150, 83, 0.2)",
                                             borderColor: "rgb(111, 207, 151)",
-                                            data: [1158, 1580, 1536, 1102, 1200, 1308],
+                                            data: @json($data[3]),
                                             fill: true,
                                         }, ],
                                     };
@@ -95,12 +95,12 @@
                 <div class="row">
                     <div class="col-md-6 col-12 ps-0 mb-2">
                         <div class="card card-saldo p-2">
-                            <h6 class="card-title">Saldo Kas</h6>
+                            <h6 class="card-title">Jumlah Anggota</h6>
                             <div class="card-body p-2">
                                 <h1 class="align-middle text-end">
-                                    <b>Rp. 3.201.098</b>
+                                    <b>{{ $data[4]->get(0)->jumlah }}</b>
                                 </h1>
-                                <h6 class="text-end">&emsp;</h6>
+                                <h6 class="text-end">Anggota</h6>
                             </div>
                         </div>
                     </div>
@@ -109,9 +109,9 @@
                             <h6 class="card-title">Anggaran Listrik</h6>
                             <div class="card-body p-2">
                                 <h1 class="align-middle text-end">
-                                    <b>Rp. 3.201.098</b>
+                                    <b>Rp. {{ $data[5]->anggaran }}</b>
                                 </h1>
-                                <h6 class="text-end">Juni 2021</h6>
+                                <h6 class="text-end">{{ date('F', mktime(0, 0, 0, $data[5]->bulan , 10)) }} {{ $data[5]->tahun }}</h6>
                             </div>
                         </div>
                     </div>
@@ -123,70 +123,25 @@
                 <div class="card card-pengumuman p-3">
                     <h6 class="card-title">Pengumuman</h6>
                     <div class="card-body p-2 overflow-auto">
-                        <div class="accordion" id="accordionExample">
+                        <div class="accordion" id="accordionPengumuman">
+                            @foreach($data[6] as $p)
                             <div class="accordion-item">
-                                <h2 class="accordion-header" id="headingOne">
-                                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
-                                        Accordion Item #1
+                                <h2 class="accordion-header" id="heading{{ $p->id }}">
+                                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse{{ $p->id }}" aria-expanded="false" aria-controls="collapse{{ $p->id }}">
+                                        {{ $p->judul }}
                                     </button>
                                 </h2>
-                                <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                                <div id="collapse{{ $p->id }}" class="accordion-collapse collapse" aria-labelledby="heading{{ $p->id }}" data-bs-parent="#accordionPengumuman">
                                     <div class="accordion-body">
-                                        <strong>This is the first item's accordion body.</strong>
-                                        It is shown by default, until the collapse plugin adds
-                                        the appropriate classes that we use to style each
-                                        element. These classes control the overall appearance,
-                                        as well as the showing and hiding via CSS transitions.
-                                        You can modify any of this with custom CSS or overriding
-                                        our default variables. It's also worth noting that just
-                                        about any HTML can go within the
-                                        <code>.accordion-body</code>, though the transition does
-                                        limit overflow.
+                                        {{ $p->isi }}
+                                        @if(!is_null($p->file))
+                                        <br><a href="{{ $p->file }}">Unduh file</a>
+                                        @endif
+
                                     </div>
                                 </div>
                             </div>
-                            <div class="accordion-item">
-                                <h2 class="accordion-header" id="headingTwo">
-                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                                        Accordion Item #2
-                                    </button>
-                                </h2>
-                                <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
-                                    <div class="accordion-body">
-                                        <strong>This is the second item's accordion body.</strong>
-                                        It is hidden by default, until the collapse plugin adds
-                                        the appropriate classes that we use to style each
-                                        element. These classes control the overall appearance,
-                                        as well as the showing and hiding via CSS transitions.
-                                        You can modify any of this with custom CSS or overriding
-                                        our default variables. It's also worth noting that just
-                                        about any HTML can go within the
-                                        <code>.accordion-body</code>, though the transition does
-                                        limit overflow.
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="accordion-item">
-                                <h2 class="accordion-header" id="headingThree">
-                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                                        Accordion Item #3
-                                    </button>
-                                </h2>
-                                <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
-                                    <div class="accordion-body">
-                                        <strong>This is the third item's accordion body.</strong>
-                                        It is hidden by default, until the collapse plugin adds
-                                        the appropriate classes that we use to style each
-                                        element. These classes control the overall appearance,
-                                        as well as the showing and hiding via CSS transitions.
-                                        You can modify any of this with custom CSS or overriding
-                                        our default variables. It's also worth noting that just
-                                        about any HTML can go within the
-                                        <code>.accordion-body</code>, though the transition does
-                                        limit overflow.
-                                    </div>
-                                </div>
-                            </div>
+                            @endforeach
                         </div>
                     </div>
                 </div>
